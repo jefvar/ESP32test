8693f94b7bde299bcd0232b7d96e50e6ad375e5f
Crysthian Salvador <crysthian_99@hotmail.com>
2024-03-08 17:27:49 +0100

    mmmmmmmmmm

--- src/main.c
+++ src/main.c
@@ -28,5 +28,31 @@ void app_main() {
     printf("Prueba motores con esP\n");
     //kp=10
     printf("Hola Mundo Mecatronico\n");
+    printf("Hola Mundo\n");
+    printf("Hola Mundo Mecatronico\n");
+    printf("xxx\n");
+    printf("Hola Mundo\n");
+    printf("Hola Mundo Mecatronico\n");
+    printf("xxx\n");
+    printf("tmr wee\n");
+    printf("Crysthian\n");
+    printf("HMI incoming data \n");
+    // ignorar 
+    printf("Prueba motores con esP\n");
+    //kp=10
+    printf("Hola Mundo Mecatronico\n");
+    printf("Hola Mundo\n");
+    printf("Hola Mundo Mecatronico\n");
+    printf("xxx\n");
+    printf("Hola Mundo\n");
+    printf("Hola Mundo Mecatronico\n");
+    printf("xxx\n");
+    printf("tmr wee\n");
+    printf("Crysthian\n");
+    printf("HMI incoming data \n");
+    // ignorar 
+    printf("Prueba motores con esP\n");
+    //kp=10
+    printf("Hola Mundo Mecatronico\n");
     
 }
